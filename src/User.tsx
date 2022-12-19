10ffe4181482fb33e5fdc2b29c9a1a6248042a74
silnex <silnex@silnex.kr>
2022-12-19 22:01:59 +0900

    use react-async

--- src/User.tsx
+++ src/User.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
-import useAsync from "./useAsync";
+import { useAsync } from "react-async";
 
-async function getUser(id: any) {
+async function getUser({ id }: any) {
   const response = await axios.get(
     `https://jsonplaceholder.typicode.com/users/${id}`
   );
@@ -10,10 +10,17 @@ async function getUser(id: any) {
 }
 
 function User({ id }: any) {
-  const [state] = useAsync(() => getUser(id), [id]);
-  const { loading, data: user, error } = state;
+  const {
+    data: user,
+    error,
+    isLoading,
+  } = useAsync({
+    promiseFn: getUser,
+    id,
+    watch: id,
+  });
 
-  if (loading) return <div>로딩중..</div>;
+  if (isLoading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다</div>;
   if (!user) return null;
 
